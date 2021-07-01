e8e8e8d544ffca8b1753fcbcfec0317f89dadcf8
Natalia Kazakova <17986517+natakazakova@users.noreply.github.com>
2021-07-01 16:22:26 +0300

    Update Form1.cs

--- CS/Dashboard_ConfigureDataConnection/Form1.cs
+++ CS/Dashboard_ConfigureDataConnection/Form1.cs
@@ -1,23 +1,18 @@
-﻿using DevExpress.DataAccess.ConnectionParameters;
+using DevExpress.DataAccess.ConnectionParameters;
 using DevExpress.DashboardCommon;
 using DevExpress.XtraEditors;
 
-namespace Dashboard_ConfigureDataConnection
-{
-    public partial class Form1 : XtraForm
-    {
-        public Form1()
-        {
+namespace Dashboard_ConfigureDataConnection {
+    public partial class Form1 : XtraForm {
+        public Form1() {
             InitializeComponent();
             dashboardViewer1.ConfigureDataConnection += dashboardViewer1_ConfigureDataConnection;
             dashboardViewer1.LoadDashboard(@"..\..\Data\nwindDashboard.xml");
         }
 
         private void dashboardViewer1_ConfigureDataConnection(object sender,
-            DashboardConfigureDataConnectionEventArgs e)
-        {
-            if (e.DataSourceName == "SQL Data Source 1")
-            {
+            DashboardConfigureDataConnectionEventArgs e) {
+            if (e.DataSourceName == "SQL Data Source 1") {
                 Access97ConnectionParameters parameters =
                     (Access97ConnectionParameters)e.ConnectionParameters;
                 parameters.UserName = "Admin";
