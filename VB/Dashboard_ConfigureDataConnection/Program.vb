b80ebb9d126de66de94713c2fcb3142fb132f398
DevExpressExampleBot <fakeEmail>
2023-10-23 18:32:41 +0400

    Source auto update [skip ci]

--- VB/Dashboard_ConfigureDataConnection/Program.vb
+++ VB/Dashboard_ConfigureDataConnection/Program.vb
@@ -1,19 +1,18 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Windows.Forms
 
 Namespace Dashboard_ConfigureDataConnection
-	Friend NotInheritable Class Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		Private Sub New()
-		End Sub
-		<STAThread> _
-		Shared Sub Main()
-			Application.SetCompatibleTextRenderingDefault(False)
-			Application.EnableVisualStyles()
-			Application.Run(New Form1())
-		End Sub
-	End Class
+
+    Friend Module Program
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            Application.SetCompatibleTextRenderingDefault(False)
+            Call Application.EnableVisualStyles()
+            Call Application.Run(New Form1())
+        End Sub
+    End Module
 End Namespace
